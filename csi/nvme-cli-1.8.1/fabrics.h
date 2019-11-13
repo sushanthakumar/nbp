88069b48fcde5ece3a012bbc964a0099ba84ba20
sushanthakumar <sushanthakumar@huawei.com>
2019-11-13 17:51:45 +0530

    Removal of not required files

--- csi/nvme-cli-1.8.1/fabrics.h
+++
@@ -1,9 +0,0 @@
-#ifndef _DISCOVER_H
-#define _DISCOVER_H
-
-extern int discover(const char *desc, int argc, char **argv, bool connect);
-extern int connect(const char *desc, int argc, char **argv);
-extern int disconnect(const char *desc, int argc, char **argv);
-extern int disconnect_all(const char *desc, int argc, char **argv);
-
-#endif
--- csi/nvme-cli-1.8.1/nvme-models.h
+++
@@ -1,6 +0,0 @@
-#ifndef NVME_MODEL_H
-#define NVME_MODEL_H
-
-char *nvme_product_name(int id);
-
-#endif
